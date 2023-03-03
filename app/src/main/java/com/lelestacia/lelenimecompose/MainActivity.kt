734dcffadab7a2c4a2b125bb73f7b43f047c1f51
kamil <km8003296@gmail.com>
2023-03-03 14:23:47 +0700

    Initial Commit

---
+++ app/src/main/java/com/lelestacia/lelenimecompose/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.lelestacia.lelenimecompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lelestacia.lelenimecompose.ui.theme.LelenimeComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LelenimeComposeTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LelenimeComposeTheme {
+        Greeting("Android")
+    }
+}
