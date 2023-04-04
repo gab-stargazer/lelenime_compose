23682a82035bd966ae9f5d39e784e72d7dda39b7
kamil <km8003296@gmail.com>
2023-04-04 21:38:09 +0700

    Added developer information

--- feature/more/src/main/java/com/lelestacia/more/screen/about/AboutScreen.kt
+++ feature/more/src/main/java/com/lelestacia/more/screen/about/AboutScreen.kt
@@ -1,28 +1,13 @@
 package com.lelestacia.more.screen.about
 
-import android.content.Intent
-import android.net.Uri
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -33,10 +18,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -44,16 +26,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import coil.compose.AsyncImage
-import com.lelestacia.common.R.*
-import com.lelestacia.common.R.drawable.github
+import com.lelestacia.more.component.DeveloperCard
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AboutScreen(
     navController: NavHostController
 ) {
-    val context = LocalContext.current
     Scaffold(
         topBar = {
             TopAppBar(
@@ -83,7 +62,7 @@ fun AboutScreen(
                 .padding(paddingValue)
         ) {
             Image(
-                painter = painterResource(id = drawable.lelenime),
+                painter = painterResource(id = com.lelestacia.common.R.drawable.lelenime),
                 contentDescription = "Application Icon",
                 contentScale = ContentScale.Inside,
                 modifier = Modifier
@@ -117,69 +96,30 @@ fun AboutScreen(
                     fontWeight = FontWeight.Bold
                 )
             )
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 24.dp, vertical = 8.dp)
-                    .clip(RoundedCornerShape(16.dp))
-                    .background(
-                        color = MaterialTheme.colorScheme.onBackground.copy(
-                            alpha = 0.1f
-                        )
-                    )
-                    .padding(horizontal = 16.dp, vertical = 8.dp),
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                AsyncImage(
-                    model = "https://avatars.githubusercontent.com/u/85838849?v=4",
-                    contentDescription = "Developer Github Profile Picture",
-                    modifier = Modifier
-                        .size(64.dp)
-                        .clip(CircleShape)
-                         .border(BorderStroke(1.dp, MaterialTheme.colorScheme.primary), CircleShape)
-                )
 
-                Column {
-                    Text(
-                        text = "LeleStacia",
-                        style = MaterialTheme.typography.titleMedium.copy(
-                            fontWeight = FontWeight.SemiBold
-                        )
-                    )
-                    Text(text = "Kamil-Malik", style = MaterialTheme.typography.titleSmall)
-                    Button(
-                        onClick = {
-                            val intent = Intent(Intent.ACTION_VIEW)
-                            intent.data = Uri.parse("https://github.com/Kamil-Malik/")
-                            context.startActivity(intent)
-                         },
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = Color.Transparent,
-                            contentColor =
-                            if (isSystemInDarkTheme()) Color.White
-                            else Color.Black
-                        ),
-                        contentPadding = PaddingValues(horizontal = 4.dp),
-                        shape = RoundedCornerShape(4.dp)
-                    ) {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Icon(
-                                painter = painterResource(id = github),
-                                contentDescription = "Github Icon",
-                                modifier = Modifier.size(24.dp)
-                            )
-                            Spacer(modifier = Modifier.size(8.dp))
-                            Text(
-                                text = "Github",
-                                style = MaterialTheme.typography.titleSmall,
-                            )
-                        }
-                    }
-                }
-            }
+            DeveloperCard(
+                name = "Lelestacia as Programmer",
+                nickName = "Kamil-Malik",
+                imageURL = "https://avatars.githubusercontent.com/u/85838849?v=4",
+                githubURL = "https://github.com/Kamil-Malik/",
+                facebookURL = "https://www.facebook.com/KamilGC"
+            )
+
+            DeveloperCard(
+                name = "Kaori Miyazono as Consultant",
+                nickName = "Kao chan",
+                imageURL = "https://scontent.fcgk6-2.fna.fbcdn.net/v/t39.30808-6/292270909_895812138479977_1493775890634793840_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGO4detcXf77S1uKx7-L31yDhwPM2rS1jcOHA8zatLWN8oL2o9NlrojoSslVUH7W2xm1aBbQ_vwN7icyoM-q5Pe&_nc_ohc=rEJLvEUr6ywAX8HzQNF&_nc_ht=scontent.fcgk6-2.fna&oh=00_AfD8XlZc-mfxDbbSVFm7vqzwrPBeCpqlVZIkqOmXdSreTA&oe=643055C0",
+                githubURL = null,
+                facebookURL = "https://www.facebook.com/kaori.miyazono.37266136"
+            )
+
+            DeveloperCard(
+                name = "Chat GPT as Consultant",
+                nickName = "GPT3",
+                imageURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUIwD84MUO1g9n6U0VWNJKRK0pPFVGTXsBeQ3KTeeGTpxX7VKB3-rMoW1J2bvU2blIFiM&usqp=CAU",
+                githubURL = null,
+                facebookURL = null,
+            )
         }
     }
 }
--- feature/more/src/main/java/com/lelestacia/more/screen/more/MoreScreen.kt
+++ feature/more/src/main/java/com/lelestacia/more/screen/more/MoreScreen.kt
@@ -1,31 +1,18 @@
 package com.lelestacia.more.screen.more
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Info
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.lelestacia.common.route.Screen
+import com.lelestacia.more.component.MoreScreenButton
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MoreScreen(
     navController: NavHostController,
@@ -35,34 +22,21 @@ fun MoreScreen(
         modifier = modifier
             .fillMaxSize()
     ) {
-        Card(
+        MoreScreenButton(
+            text = "Settings",
+            icon = Icons.Filled.Settings
+        ) {
+
+        }
+        MoreScreenButton(
+            text = "About",
+            icon = Icons.Filled.Info,
             onClick = {
                 navController.navigate(route = Screen.About.route) {
                     restoreState = true
                 }
-            },
-            colors = CardDefaults.cardColors(
-                containerColor = Color.Transparent
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
-                modifier = Modifier.padding(
-                    horizontal = 16.dp,
-                    vertical = 24.dp
-                )
-            ) {
-                Icon(imageVector = Icons.Outlined.Info, contentDescription = "About")
-                Text(
-                    text = "About",
-                    fontWeight = FontWeight.SemiBold
-                )
             }
-            Divider()
-        }
+        )
     }
 }
 
